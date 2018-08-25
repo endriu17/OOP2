b2d0378ba70fb7f108584f86c27fc90d4a6cbc19
endriu17 <aolaszewski@gmail.com>
2018-08-25 15:42:50 +0200

    fix self :)

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function Button(text){
 
 Button.prototype = {
     create: function(){
+        var self = this;
         this.$element = $('<button>');
         this.$element.text(this.text);
         this.$element.click(function(){
